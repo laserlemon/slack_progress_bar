f55d1b905f5f4cd0963fb1dcb9e9402547fd6e5f
Steve Richert <steve.richert@gmail.com>
2021-06-21 10:16:29 -0400

    Add CLI option descriptions

--- lib/slack_progress_bar/cli.rb
+++ lib/slack_progress_bar/cli.rb
@@ -12,12 +12,15 @@ class SlackProgressBar
     desc "generate",
       "Generate Slack emoji images"
     option :colors,
+      desc: "A collection of key/value pairs where the keys are letters used for image file naming and the values are hex colors used for image generation",
       type: :hash,
       default: defaults.colors
     option :prefix,
+      desc: "A string used as the prefix for every generated image file name",
       type: :string,
       default: defaults.prefix
     option :output,
+      desc: "The directory where all generated image files will be saved",
       type: :string,
       default: defaults.output
 
